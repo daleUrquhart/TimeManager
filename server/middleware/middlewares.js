d055495f6d36e69948e5ef5e5aa2bb910437157e
daleUrquhart <daleurquhart1@gmail.com>
2024-06-24 12:25:16 -0300

    Update middlewares.js

--- server/middleware/middlewares.js
+++ server/middleware/middlewares.js
@@ -1,6 +1,6 @@
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { sequelize } = require('./db');
+const { sequelize } = require('../db');
 
 const store = new SequelizeStore({
     db: sequelize
