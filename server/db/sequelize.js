12b15dbbd6d6446dd4c2582a90cbd703270bd642
daleUrquhart <daleurquhart1@gmail.com>
2024-06-24 18:48:13 -0300

    Updated DB HOST

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
  
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.JAWS_DB,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false, 
 });
